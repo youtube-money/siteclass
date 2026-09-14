49c21f9dab1ba562a6fb61fef020b4a7c9b85d84
youtube-money <gemenai12398@gmail.com>
2026-09-14 10:12:01 +0330

    Clear site session cookie on logout

--- api/logout.php
+++ api/logout.php
@@ -3,6 +3,17 @@ require_once __DIR__ . '/session-helper.php';
 
 startSecureSession();
 $_SESSION = [];
+
+$params = session_get_cookie_params();
+setcookie(session_name(), '', [
+    'expires' => time() - 42000,
+    'path' => $params['path'] ?? '/',
+    'domain' => $params['domain'] ?? '',
+    'secure' => (bool)($params['secure'] ?? false),
+    'httponly' => (bool)($params['httponly'] ?? true),
+    'samesite' => $params['samesite'] ?? 'Lax',
+]);
+
 session_destroy();
 
 jsonResponse(['success' => true]);
